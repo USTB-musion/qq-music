28bcf4c4e7a2e4894ee7586ad73dbd228a1c6488
JeeLee <18810996718@163.com>
2017-08-20 16:44:41 +0800

    add song-list and music-list components

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="slide">
-  	<div class="singer-detail"></div>
+  	<music-list :songs="songs" :title="title" :bg-image="bgImage"></music-list>
   </transition>
 </template>
 
@@ -9,6 +9,7 @@
 	import {getSingerDetail} from 'api/singer'
 	import {ERR_OK} from 'api/config'
 	import {createSong} from 'common/js/song'
+	import MusicList from 'components/music-list/music-list'
 
 	export default {
 		data() {
@@ -17,6 +18,12 @@
 			}
 		},
 		computed: {
+			title() {
+				return this.singer.name
+			},
+			bgImage() {
+				return this.singer.avatar
+			},
 			...mapGetters([
 				'singer'
 			])
@@ -47,20 +54,13 @@
 					}
 				})
 			}
+		},
+		components: {
+			MusicList
 		}
 	} 
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
-
-  .singer-detail 
-  	position: fixed
-  	z-index: 100
-  	top: 0
-  	left: 0
-  	right: 0
-  	bottom: 0
-  	background: $color-background
-
 </style>
