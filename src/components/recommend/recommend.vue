a6c4229aac9d80d286e484fcc7870585cd0c0541
JeeLee <18810996718@163.com>
2017-09-13 17:30:22 +0800

    add disc components and update recommend.vue

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,85 +1,103 @@
 <template>
-  <div class="recommend">
-	<scroll class="recommend-content" :data="discList" ref="scroll">
-	  <div v-if="recommends.length" class="slider-wrapper">
-	  	<slider>
-	  	  <div v-for="item in recommends">
-	  	  	<a :href="item.linkUrl">
-	  	  	  <img class="needsclick" @load="loadImage" :src="item.picUrl">
-	  	  	</a>
-	  	  </div>
-	  	</slider>
-	  </div>
-	  <div class="recommend-list">
-	  	<h1 class="list-title">热门歌单推荐</h1>
-	  	<ul>
-	  	  <li v-for="item in discList" class="item">
-	  	  	<div class="icon">
-	  	  	  <img width="60" height="60" v-lazy="item.imgurl" >
-	  	  	</div>
-	  	  	<div class="text">
-	  	  	  <h2 class="name" v-html="item.creator.name"></h2>
-	  	  	  <p class="desc" v-html="item.dissname"></p>
-	  	  	</div>
-	  	  </li>
-	  	</ul>
-	  </div>
-	  <div class="loading-container" v-show="!discList.length">
-	  	<loading></loading>
-	  </div>
-	</scroll>
+  <div class="recommend" ref="recommend">
+    <scroll ref="scroll" class="recommend-content" :data="discList">
+      <div>
+        <div v-if="recommends.length" class="slider-wrapper" ref="sliderWrapper">
+          <slider>
+            <div v-for="item in recommends">
+              <a :href="item.linkUrl">
+                <img class="needsclick" @load="loadImage" :src="item.picUrl">
+              </a>
+            </div>
+          </slider>
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li @click="selectItem(item)" v-for="item in discList" class="item">
+              <div class="icon">
+                <img width="60" height="60" v-lazy="item.imgurl">
+              </div>
+              <div class="text">
+                <h2 class="name" v-html="item.creator.name"></h2>
+                <p class="desc" v-html="item.dissname"></p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="loading-container" v-show="!discList.length">
+        <loading></loading>
+      </div>
+    </scroll>
+    <router-view></router-view>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import Slider from 'base/slider/slider'
   import Loading from 'base/loading/loading'
   import Scroll from 'base/scroll/scroll'
-  import Slider from 'base/slider/slider'
-  import {getRecommend,getDiscList} from 'api/recommend'
+  import {getRecommend, getDiscList} from 'api/recommend'
+  import {playlistMixin} from 'common/js/mixin'
   import {ERR_OK} from 'api/config'
+  import {mapMutations} from 'vuex'
 
-  export default{
-  	data() {
-  	  return {
-  	  	recommends: [],
-  	  	discList: []
-  	  }
-  	},
-  	created() {
-	  
-	  this._getDiscList()
-	  this._getRecommend()
-	
-  	  
-  	},
-  	methods: {
-  	  _getRecommend() {
-  	  	getRecommend().then((res) => {
-	  	  if(res.code === ERR_OK){
-	  	  	 this.recommends = res.data.slider
-	  	  	}
-  	  	})
-  	  },
-  	  _getDiscList() {
-  	  	getDiscList().then((res) => {
-	  	  if(res.code === ERR_OK){
-	  	  	 this.discList = res.data.list
-	  	  	}
-  	  	})
-  	  },
-  	  loadImage() {
-  	  	if(!this.checkLoaded) {
-  	  	  this.$refs.scroll.refresh()
-  	  	  this.checkLoaded = true
-  	  	}
-  	  }
-  	},  
-  	components: {
-  	  Slider,
-  	  Scroll,
-  	  Loading
-  	}
-  
+  export default {
+    mixins: [playlistMixin],
+    data() {
+      return {
+        recommends: [],
+        discList: []
+      }
+    },
+    created() {
+      this._getRecommend()
+
+      this._getDiscList()
+    },
+    methods: {
+      handlePlaylist(playlist) {
+        const bottom = playlist.length > 0 ? '60px' : ''
+
+        this.$refs.recommend.style.bottom = bottom
+        this.$refs.scroll.refresh()
+      },
+      loadImage() {
+        if (!this.checkloaded) {
+          this.checkloaded = true
+          this.$refs.scroll.refresh()
+        }
+      },
+      selectItem(item) {
+        this.$router.push({
+          path: `/recommend/${item.dissid}`
+        })
+        this.setDisc(item)
+      },
+      _getRecommend() {
+        getRecommend().then((res) => {
+          if (res.code === ERR_OK) {
+            this.recommends = res.data.slider
+          }
+        })
+      },
+      _getDiscList() {
+        getDiscList().then((res) => {
+          if (res.code === ERR_OK) {
+            this.discList = res.data.list
+          }
+        })
+      },
+      ...mapMutations({
+        setDisc: 'SET_DISC'
+      })
+    },
+    components: {
+      Slider,
+      Loading,
+      Scroll
+    }
   }
 </script>
 
